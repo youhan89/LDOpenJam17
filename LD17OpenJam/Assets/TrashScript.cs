6c313fa39abc51b3edb0ca9dc2a5206d9de5efad
Youhan <johan.sogaard@gmail.com>
2017-10-06 21:34:10 +0200

    Initial commit

---
+++ LD17OpenJam/Assets/TrashScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour {
+
+
+	private float originY;
+	public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		this.originY = this.transform.position.y;
+	}
+
+	void ResetTrash() {
+		rb2d.velocity = new Vector3(0f,0f,0f);
+		this.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), originY, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.y < -10) {
+			ResetTrash();
+		}
+	}
+}
