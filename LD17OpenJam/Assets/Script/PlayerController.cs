2e7acbf5f18886f1afebd64fda286fcd1fa629e5
Youhan <johan.sogaard@gmail.com>
2017-10-06 22:29:51 +0200

    Additional fixes

---
+++ LD17OpenJam/Assets/Script/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, transform.position.y);
+     	transform.position = (pos);
+	}
+}
