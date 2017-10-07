94b1e80a0480a5a3c8b60055a9d91b558f060c68
Youhan <johan.sogaard@gmail.com>
2017-10-07 14:32:24 +0200

    Added MainMenu

---
+++ LD17OpenJam/Assets/MenuControllerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void LoadGame() {
+		SceneManager.LoadScene("Main");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
