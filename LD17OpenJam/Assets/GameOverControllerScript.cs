4b2223fd72e9c092ff3034fb539bd263f1e05a2a
Youhan <johan.sogaard@gmail.com>
2017-10-07 14:53:53 +0200

    Added MainMenu & Gameover menu

---
+++ LD17OpenJam/Assets/GameOverControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void LoadGame() {
+		SceneManager.LoadScene("Main");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
