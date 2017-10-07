e890db35d1ce82b6582772462dddcdccc1515e79
Youhan <johan.sogaard@gmail.com>
2017-10-07 14:08:31 +0200

    Spacetheme

--- LD17OpenJam/Assets/Script/TrashScript.cs
+++ LD17OpenJam/Assets/Script/TrashScript.cs
@@ -17,7 +17,7 @@ public class TrashScript : MonoBehaviour {
 	void ResetTrash() {
 		rb2d.velocity = new Vector3(0f,0f,0f);
 		rb2d.AddTorque(Random.Range(-20.0f, 20.0f));
-		this.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), originY, 0);
+		this.transform.position = new Vector3(Random.Range(-6.0f, 6.0f), originY, 0);
 	}
 	
 	// Update is called once per frame
