8d64805db78bde24101086c7655f8886f603ddbc
Youhan <johan.sogaard@gmail.com>
2017-10-08 07:19:30 +0200

    Added earth spinning in the background of menues

---
+++ LD17OpenJam/Assets/EarthController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthController : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.AddTorque(3.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
